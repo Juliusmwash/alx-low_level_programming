fdd56f43b1c2394382c939723539a4d8805529ef
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-08 16:21:07 -0800

    Make a script 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+nclude <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#define STRING "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+/**
+ * * main - Entry point
+ * *
+ * * Return: Always 0 (Success)
+ * */
+int main(void)
+{
+	int len = strlen(STRING);
+	write(1, STRING, len);
+	return (1);
+}
