bf7bdaf0a2e0b498f5d1ca6d70dd1996ccb85cf4
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-10 15:45:24 -0800

    Make a 8-print_base16.c program

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+	for (c = 'a'; c <= 'f'; c++)
+		putchar(c);
+	putchar('\n');
+
+	return (0);
+}
