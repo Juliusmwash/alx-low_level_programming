74815a4aeb223a36fd210246d291018a75c0bf04
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-10 13:26:06 -0800

    Make 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+	{
+		if(c == 'e' || c == 'q')
+			continue;
+		else
+			putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
