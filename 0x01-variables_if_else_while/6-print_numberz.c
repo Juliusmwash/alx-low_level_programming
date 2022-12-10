784c9c184250d27de1894c9d253043346b1c3748
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-10 14:51:14 -0800

    Make 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,16 @@
 #include <stdio.h>
 
-/**
+/*
  * main - Entry
  *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	int i;
-	char c [10] = {"0123456789"};
+	long int g;
 
-	for (i = 0; i < 10; i++)
-		putchar(c[i]);
+	for (g = '0'; g <= '9'; g++)
+		putchar(g);
 	putchar('\n');
 
 	return (0);
