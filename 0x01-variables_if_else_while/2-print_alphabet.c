5cd01aa7cf9775bf3680c7344381f9cf67bcc44a
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-10 12:25:27 -0800

    Make a 2-print_alphabet.c program

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	char c;
+	for (c = 'a';c <= 'z';c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
+}
