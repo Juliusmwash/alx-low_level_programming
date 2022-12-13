241c31b273055c89df4573e9de028bf9d5dcaa46
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-13 08:28:35 -0800

    Remake 1-alphabet.c and Make print_alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,17 +7,8 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet()
-{
-	char c;
-
-	for (c = 'a'; c < 'z'; c++)
-		_putchar(c);
-	_putchar(\n);
-}
-
 int main(void)
 {
 	print_alphabet();
-	return (0;
+	return (0);
 }
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,8 @@
+void print_alphabet()
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
