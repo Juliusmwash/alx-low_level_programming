b71fd40cc603e370c18961b04cf5b70982a003a1
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-13 12:37:58 -0800

    Make 2-print_alphabet_x10.c file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabets from a to z 10 times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+	{
+		int d;
+
+		for (d = 'a'; c <= 'z'; c++)
+			_putchar(d);
+	}
+	_putchar('\n');
+	return (0);
+}
