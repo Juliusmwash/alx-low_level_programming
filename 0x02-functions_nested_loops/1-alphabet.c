12aa33e5405d46ad62aefdd844fd596fb5089b41
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-13 09:01:09 -0800

    Trying to work things out

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,15 +7,6 @@
  * Return: Always 0 (Success)
  */
 
-void print_alphabet()
-{
-	char c;
-
-	for (c = 'a'; c < 'z'; c++)
-		_putchar(c);
-	_putchar('\n');
-}
-
 int main(void)
 {
 	print_alphabet();
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,9 @@
+
+void print_alphabet()
+{
+	char c;
+
+	for (c = 'a'; c < 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
