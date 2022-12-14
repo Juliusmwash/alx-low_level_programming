f3febdbd8ca906fe593b2517c229efb00e0a8b53
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-15 00:26:10 +0300

    Make a 10-add.c and add a prototype to main .h

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - returns sum of two ints
+ * @a: first number
+ * @b: second number
+ *
+ * Return: Always 0
+ */
+int add(int a, int b)
+{
+	int sum;
+
+	sum = a + b;
+	return (sum);
+}
