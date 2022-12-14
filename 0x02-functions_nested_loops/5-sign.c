6a439e5faff5e6113f945f80a67a59c7efd3d038
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-14 15:17:16 +0300

    Make 5-sign.c file and add a prototype to main.h

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_sign - prints sign depending on the numbers
+ * @c: number used to determine the sign to print
+ *
+ * Return: Always 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (0);
+	}
+}
