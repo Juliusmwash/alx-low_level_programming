a8faebd4ddc593a1cdeb3104de65c07abda45fa7
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-14 18:01:12 +0300

    Trying

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,14 +10,15 @@ void jack_bauer(void)
 	int min;
 
 	for (hour = 0; hour <= 23; hour++)
+	{
+		for (min = 0; min <= 59; min++)
 		{
-			for (min = 0; min <= 59; min++)
-			{
-				_putchar(hour / 10 + '0');
-			}	_putchar(hour % 10 + '0');
-				_putchar(':');
-				_putchar(min / 10 + '0');
-				_putchar(min % 10 + '0');
-				_putchar('\n');
-			}
+			_putchar(hour / 10 + '0');
+			_putchar(hour % 10 + '0');
+			_putchar(':');
+			_putchar(min / 10 + '0');
+			_putchar(min % 10 + '0');
+			_putchar('\n');
 		}
+	}
+}
