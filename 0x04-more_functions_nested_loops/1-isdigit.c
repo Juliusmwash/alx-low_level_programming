dd4e687dd1acca23fa01fe4e1c9db106e551c59e
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-15 04:59:42 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for digit characters
+ * @c: character being checked
+ *
+ * Return: Always 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
