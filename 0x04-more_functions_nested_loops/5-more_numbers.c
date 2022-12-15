a4a058832b59e53841abcf3f39ea2bd463eb4ce4
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-15 06:18:50 -0800

    Task 6

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,13 @@ void more_numbers(void)
 	{
 		for (b = 0; b < 15; b++)
 		{
+			if (b <= 9)
 			_putchar(b + '0');
+			else
+			{
+			_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+			}
 		}
 		_putchar(10);
 	}
