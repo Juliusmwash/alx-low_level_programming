5073d0fa04da12bf0992ceb8aab512b5163fc32a
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-16 02:33:20 +0300

    Task 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -8,4 +8,4 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
