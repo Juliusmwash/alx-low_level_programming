ffed88badf58c5ad890fa664f38a1ba115a9b9c8
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-15 05:37:44 -0800

    Task 4

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * print_numbers - prints numbers form 1 to 9
- * 
+ *
  * Return: Always 0
  */
 void print_numbers(void)
 {
 	int a;
+
 	for (a = 48; a <= 57; a++)
 		_putchar(a);
 	_putchar(10);
