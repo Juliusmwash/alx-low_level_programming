4cbb7de8cba6d478bdcb043cb6f4453d74dc9bb9
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-16 02:36:49 +0300

    Task 2

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,7 +10,6 @@ int main(void)
 	int i;
 
 	printf("Infinite loop incoming :(\n");
-
 	i = 0;
 	/*
 	*while (i < 10)
@@ -19,6 +18,5 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-
 	return (0);
 }
