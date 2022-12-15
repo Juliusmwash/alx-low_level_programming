1d031b47b9005f229fbb3a9a5203d72a283338f5
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-14 17:37:08 -0800

    Make a 102-fibonacci.c program

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	int c = 0;
+	int d;
+
+	for (d = 0; d < 50; a++)
+	{
+		c = a + b;
+		printf("%d", c);
+		a = b;
+		b = c;
+		if (d != 49)
+		{
+			printf(',');
+		}
+		else
+		{
+			printf('\n');
+		}
+	}
+	return (0);
+}
+
