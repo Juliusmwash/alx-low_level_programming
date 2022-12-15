f57c13b9280c086d84ce41c3918f720136366fe6
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-15 05:24:45 -0800

    Task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers form 1 to 9
+ * 
+ * Return: Always 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+		_putchar(a + '0');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,5 @@
 int _isupper(int c);
 int _isdigit(int);
 int mul(int, int);
+int _putchar(char);
+void print_numbers(void);
