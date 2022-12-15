b57c6c3d50f4216c1a55eda0cb97d77087b67829
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-15 12:28:52 -0800

    Task 10

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,17 +10,30 @@ int main(void)
 
 	while (a <= 100)
 	{
+		if (a < 100)
+		{
 		if (a % 3 == 0 && a % 5 == 0)
 			printf("FizzBuzz ");
 		else if (a % 3 == 0)
 			printf("Fizz ");
-		else if (a % 5 == 0 && a != 100)
+		else if (a % 5 == 0)
 			printf("Buzz ");
-		else if (a % 5 == 0 && a == 100)
-			printf("Buzz");
 		else
 			printf("%d ");
+		}
+		else
+		{
+		if (a % 3 == 0 && a % 5 == 0)
+			printf("FizzBuzz");
+		else if (a % 3 == 0)
+			printf("Fizz");
+		else if (a % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d");
+		}
 		a++;
 	}
+	printf("\n");
 	return (0);
 }
