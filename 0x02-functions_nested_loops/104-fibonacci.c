c3edb1faddc4ba78fc25df27e5901b7e93f61880
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-16 03:26:25 +0300

    Make a 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -10,17 +10,22 @@ int main(void)
 	long int b = 1;
 	long int c = 0;
 	long int sum = 0;
+	int d = 0;
 
 	while (c < 4000000)
 	{
 		c = a + b;
 		a = b;
 		b = c;
-		if (c % 2 == 0)
+		for (d = 0; d <= 98; d++)
 		{
-			sum += c;
+			if(d != 98)
+				printf("%lu, ", c);
+			else
+				printf("%lu", c);
 		}
+		if (d == 98)
+			break;
 	}
-	printf("%lu\n", sum);
 	return (0);
 }
