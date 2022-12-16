908fa8ecf8c59bb90a0afce3367f0247743085c7
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-16 04:16:54 +0300

    Task second last

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,35 +1,36 @@
 #include <stdio.h>
 #include <math.h>
-
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
 int main() {
-    // Write C code here
     int a;
     double b = 2;
     double n = 200;
     int e = 2;
     int f;
     int g;
-    printf("Enter a number : ");
-    scanf("%lf",&n);
-    printf("Prime factors of %0.0lf are :\n",n);
+
     while (e > 0)
     {
         if (remainder(n,b) == 0 && e != 1)
         {
-            n = n/b;
+            n = n / b;
             e = n;
             g = b;
-            printf("%d ",g);
         }
         else
         {
-            b+=1;
+            b += 1;
         }
         if (e == 1)
         {
             f = b;
-            printf("\nThe greatest factor is : %d",f);
+            printf("%d", f);
             break;
         }
     }
-    return 0;
+    return (0);
+}
