87a5200fe82d727c7009a28e7cde25fa8d7625d5
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-15 18:01:27 -0800

    Task second last

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -5,32 +5,34 @@
  *
  * Return: Always 0
  */
-int main() {
-    int a;
-    long double b = 2;
-    long double n = 612852475143;
-    long int e = 2;
-    long int f;
-    int g;
+int main(void)
+{
+	int a;
 
-    while (e > 0)
-    {
-        if (remainder(n,b) == 0 && e != 1)
-        {
-            n = n / b;
-            e = n;
-            g = b;
-        }
-        else
-        {
-            b += 1;
-        }
-        if (e == 1)
-        {
-            f = b;
-            printf("%lu", f);
-            break;
-        }
-    }
-    return (0);
+	long double b = 2;
+	long double n = 612852475143;
+	long int e = 2;
+	long int f;
+	int g;
+
+	while (e > 0)
+	{
+		if (remainder(n, b) == 0 && e != 1)
+		{
+			n = n / b;
+			e = n;
+			g = b;
+		}
+		else
+		{
+			b += 1;
+		}
+		if (e == 1)
+		{
+			f = b;
+			printf("%lu", f);
+			break;
+		}
+	}
+	return (0);
 }
