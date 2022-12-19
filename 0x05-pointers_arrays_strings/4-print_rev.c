f54a60fee0f4242c6952d33fd9c4961109dcd630
Juliusmwash <118174651+Juliusmwash@users.noreply.github.com>
2022-12-20 00:36:18 +0300

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,29 +1,30 @@
-include "main.h"
-
+#include "main.c"
 /**
- * rev_string - A function that reverses a string
- * @s: A string
- * Return: return nothing
- */
-
-void rev_string(char *s)
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: Always 0
+ *
+void print_rev(char *s)
 {
-	int i, max, half;
-	char first, last;
+	int a = 0;
+	int b;
+	int c;
 
-	i = 0;
-	while (s[i] != '\0')
+	while (a == 0)
 	{
-		i++;
-	}
-	max = i - 1;
-	half = max / 2;
-	while (half >= 0)
-	{
-		first = s[max - half];
-		last = s[half];
-		s[half] = first;
-		s[max - half] = last;
-		half--;
+		b = 0;
+		while (*(s + b) != '\0')
+		{
+			b += 1;
+		}
+		b = b - 1;
+		c = 0;
+		while (b >= c)
+		{
+			_putchar(*(s + b));
+			c--;
+		}
+		_putchar('\n');
 	}
 }
