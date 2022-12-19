1d4e8916b33eaa8f5c6cf046353875524a033916
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-19 15:13:19 +0300

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,30 @@
+#include "main.c"
+/**
+ * print_rev - prints a string in reverse
+ * @s: string
+ *
+ * Return: Always 0
+ */
+void print_rev(char *s)
+{
+	int a = 0;
+	int b;
+	int c;
+
+	while (a == 0)
+	{
+		b = 0;
+		while (*(s+b) != '\0')
+		{
+			b += 1;
+		}
+		b = b - 1;
+		c = 0;
+		while (b >= c)
+		{
+			_putchar(*(s+ b));
+			c--;
+		}
+		_putchar('\n');
+	}
+}
