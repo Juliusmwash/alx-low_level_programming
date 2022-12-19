ccd9e4fb6c275894b966b807e9744dfa3f42dfd3
Juliusmwash <118174651+Juliusmwash@users.noreply.github.com>
2022-12-20 01:40:01 +0300

    Main.h files

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,17 @@
-int _putchar(char c);
-void reset_to_98(int *);
-void swap_int(int *, int *);
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 int _strlen(char *s);
-void _puts(char *);
+void _puts(char *str);
+int _print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+int _atoi(char *s);
+
+#endif
