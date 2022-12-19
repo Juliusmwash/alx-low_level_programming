6492e67272f8ff53fcec77b91745d70715a0a5ca
Juliusmwash <cutejuliusmwash@gmail.com>
2022-12-19 14:35:36 +0300

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 /**
  * _puts - prints the string on the stdout
  *@s: string
-
+ *
  * Return: Always 0
  */
 void _puts(char *s)
 {
 	int len = 0;
 
-	while (*(s+len) != '\0')
+	while (*(s + len) != '\0')
 	{
 		_putchar(*(s + len));
 		len++;
