560dcb955ffa1a78d1ca12d599869acd2ab9a4b9
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-30 01:46:32 +0300

    main.h

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,11 +2,6 @@
 #define MAIN_H
 #include <stdio.h>
 
-/**
- *void prototypes()
- *int prototypes()
- *char prototypes()
- */
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
