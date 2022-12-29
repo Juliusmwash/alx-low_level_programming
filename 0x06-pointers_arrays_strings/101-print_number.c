0e5ea603dbc58801fcfc4914a05999a048284248
Julius Mwangi <cutejuliusmwash@gmail.com>
2022-12-29 22:56:43 +0300

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,16 +7,91 @@
 */
 void print_number(int n)
 {
-	unsigned int num = n;
+	int m;
 
-	if (n < 0)
+	if (n >= 0)
+	{
+	if (n < 10)
+	{
+		_putchar(n + '0');
+		_putchar(10);
+	}
+	else if (n < 100)
+	{
+		m = n / 10;
+		_putchar(m + '0');
+		m = n % 10;
+		_putchar(m + '0');
+		_putchar(10);
+	}
+	else if (n < 1000)
+	{
+		m = n / 100;
+		_putchar(m + '0');
+		m = n % 100;
+		n = m / 10;
+		_putchar(n + '0');
+		n = n % 10;
+		_putchar(n + '0');
+		_putchar(10);
+	}
+	else if (n < 10000)
+	{
+		m = n / 1000;
+		_putchar(m + '0');
+		m = n % 1000;
+		n = m / 100;
+		_putchar(n + '0');
+		n = n % 100;
+		m = n / 10;
+		_putchar(m + '0');
+		m = m % 10;
+		_putchar(m + '0');
+		_putchar(10);
+	}
+	}
+	else
 	{
 		_putchar('-');
-		num = -num;
+		m = -1 * n;
+		if (m < 10)
+		{
+			_putchar(m + '0');
+			_putchar(10);
+		}
+		else if (m < 100)
+		{
+			n = m / 10;
+			_putchar(n + '0');
+			n = m % 10;
+			_putchar(n + '0');
+			_putchar(10);
+		}
+		else if (m < 1000)
+		{
+			n = m / 100;
+			_putchar(n + '0');
+			n = m % 100;
+			m = n / 10;
+			_putchar(m + '0');
+			m = n % 10;
+			_putchar(m + '0');
+			_putchar(10);
+		}
+		else if (m < 10000)
+		{
+			n = m / 1000;
+			_putchar(n + '0');
+			n = m % 1000;
+			m = n / 100;
+			_putchar(m + '0');
+			m = n % 100;
+			n = m / 10;
+			_putchar(n + '0');
+			n = m % 10;
+			_putchar(n + '0');
+			_putchar(10);
+		}
 	}
 
-	if ((num / 10) > 0)
-		print_number(num / 10);
-
-	_putchar((num % 10) + '0');
 }
