fbffe5a8217dda6179d8a5214aad6550cd61ba44
Juliusmwash <118174651+Juliusmwash@users.noreply.github.com>
2023-01-10 00:08:11 +0300

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _abs - prints absolute value of an int
+ *
+ * @c: number to get its absolute value
+ *
+ * Return: Always 0
+ */
+int _abs(int c)
+{
+	if (c >= 0)
+		return (c);
+	else if (c < 0)
+	{
+		c = -1 * c;
+		return (c);
+	}
+	else
+	{
+		return (0);
+	}
+}
