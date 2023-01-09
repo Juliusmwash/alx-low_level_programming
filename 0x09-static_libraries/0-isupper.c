8cd9f6a045608de369bfbbfe63220dbfe84d08b2
Juliusmwash <118174651+Juliusmwash@users.noreply.github.com>
2023-01-09 23:26:51 +0300

    Create 0-isupper.c

---
+++ 0x09-static_libraries/0-isupper.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+#include "main.h"
+/**
+ * _isupper - checks for upper case characters
+ * @c: character or check
+ *
+ * Return: Always 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
