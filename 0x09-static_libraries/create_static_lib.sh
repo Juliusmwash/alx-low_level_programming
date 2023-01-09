930c9fdb0ccb7a57a904cbde296cc48ced536019
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 02:02:07 +0300

    Make create_static_lib.sh script file

---
+++ 0x09-static_libraries/create_static_lib.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+gcc -c 0-isupper.c 1-strncat.c 3-islower.c 4-strpbrk.c 0-memset.c 100-atoi.c 3-puts.c 5-strstr.c 0-strcat.c 2-strchr.c 3-strcmp.c 6-abs.c 1-isdigit.c 2-strlen.c 3-strspn.c 9-strcpy.c 1-memcpy.c 2-strncpy.c 4-isalpha.c _putchar.c
+ar rc liball.a 0-isupper.o 1-strncat.o 3-islower.o 4-strpbrk.o 0-memset.o 100-atoi.o 3-puts.o 5-strstr.o 0-strcat.o 2-strchr.o 3-strcmp.o 6-abs.o 1-isdigit.o 2-strlen.o 3-strspn.o 9-strcpy.o 1-memcpy.o 2-strncpy.o 4-isalpha.o _putchar.o
