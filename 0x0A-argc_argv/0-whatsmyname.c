b687c69209bd4a2885495638f5a831bd46ad6854
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 14:50:36 +0300

    Create 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,7 +9,16 @@ void print_name(char *s)
 {
 	printf("%s\n", s);
 }
-int main(int argc, char *argv[])
+
+/**
+ * main - calls function print_name
+ * @argc: count of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	print_name(argv[0]);
 	return (0);
