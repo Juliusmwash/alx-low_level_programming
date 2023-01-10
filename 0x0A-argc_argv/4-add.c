50c88198bf3fe1346dab4f1d662fd686a04f1dc9
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 08:32:50 -0800

    Create 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -34,7 +34,7 @@ int check_for_num(char *c)
  * @argc: number of arguments
  * @argv: array of arguments
  *
- * Return : 0 (Success), 1 (Error)
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
