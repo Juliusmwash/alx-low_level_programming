2154d20ad7690d0ce265ffb4f8f8bfd832515c53
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 16:40:04 +0300

    Create 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -10,7 +10,7 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -19,8 +19,9 @@ int main(int argc, char *argv[])
 	{
 		int n = atoi(argv[1]);
 		int n1 = atoi(argv[2]);
+		int mult = n * n1;
 
-		printf("%d\n", n * n1);
+		printf("%d\n", mult);
 	}
 	return (0);
 }
