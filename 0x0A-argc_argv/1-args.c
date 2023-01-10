e030597577cabb4427b5ca706d99532ce1d87e6c
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 15:15:56 +0300

    Create 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: number of arguments
+ * @argv: array containing arguments
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
