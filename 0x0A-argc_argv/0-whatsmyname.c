e24382230f9b628d656e74a7adb75731b318f9ae
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 13:34:41 +0300

    Create a directory 0x0A-argc_argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints the new name of the program
+ * @argc: first argument
+ * @argv: second argument
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
