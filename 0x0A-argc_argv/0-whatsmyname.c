ce9e4e8719f521b918c7b58808e06fcd4972d38c
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-10 14:37:40 +0300

    Change 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * print_name - prints the new name of the program
  * @s: first argument
@@ -9,4 +8,9 @@
 void print_name(char *s)
 {
 	printf("%s\n", s);
+}
+int main(int argc, char *argv[])
+{
+	print_name(argv[0]);
+	return (0);
 }
