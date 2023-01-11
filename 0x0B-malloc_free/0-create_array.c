b9b21828c8a5c75be8a38c44d88879c48658a6a8
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-12 00:48:38 +0300

    Correct 0-create_array.c errors (4)

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,10 +16,7 @@ char *create_array(unsigned int size, char c)
 	s = (char *)malloc(size * sizeof(unsigned int));
 	if (s == NULL || size == 0)
 		return (NULL);
-	else
-	{
-		for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 			s[i] = c;
-	}
 	return (s);
 }
