6f8ef0b8284bb2d8cd35bb28fd985f2aba3d2dfe
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-12 00:34:53 +0300

    Correct 0-create_array.c errors

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,3 +1,13 @@
+#include "main.h"
+
+/**
+ * create_array - returns a pointer to array
+ * @size: size of the dynamic array
+ * @c: character being used to fill array
+ *
+ * Return: pointer to array
+ */
+
 char *create_array(unsigned int size, char c)
 {
 	int i;
