befb6d6be0bfd3c4e160357309067ad58182ebfc
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-12 05:07:30 +0300

    Correct 2-str_concat.c errors (3)

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -17,9 +17,9 @@ char *str_concat(char *s1, char *s2)
 
 	i = 0;
 	j = 0;
-	while (*(s1 + i) != '\0')
+	while (s1 && *(s1 + i) != '\0')
 		i++;
-	while (*(s2 + j) != '\0')
+	while (s2 && *(s2 + j) != '\0')
 		j++;
 	j++;
 	k = i + j;
