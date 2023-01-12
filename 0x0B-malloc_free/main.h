e835d1311f169758f842c1d5ecdfcd7f6e0e0175
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-12 05:13:20 +0300

    Make main.h

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -7,5 +7,8 @@ char *create_array(unsigned int size, char c);
 int _putchar(char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+char **strtow(char *str);
 
 #endif
