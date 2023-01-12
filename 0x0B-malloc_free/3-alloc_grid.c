bc85ddf539b9574b8aae82713e13ba40176f632b
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-13 01:17:29 +0300

    Correct 3-alloc_grid.c errors (4);

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,29 +10,34 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **arr;
+	int **arr = NULL;
 	int i, j;
 
 	arr = malloc(sizeof(int *) * width);
 	if (arr == NULL)
 	{
-		free(arr);
 		return (NULL);
+	}
+	if (!(arr))
+	{
+		free(arr);
+		arr = NULL;
 	}
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
+	if (arr)
+	{
 	for (i = 0; i < width; i++)
 	{
 		arr[i] = malloc(sizeof(int) * height);
 		if (arr[i] == NULL)
 		{
-			free(arr[i]);
 			return (NULL);
 		}
 	}
 	for (i = 0; i < width; i++)
 		for (j = 0; j < height; j++)
 			*(*(arr + i) + j) = 0;
+	}
 	return (arr);
 }
