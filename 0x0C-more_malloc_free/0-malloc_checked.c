8795f283dd7846af7e6670efa0b4b62582cfea85
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-14 01:40:40 +0300

    Start project

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - allocates memory
+ * @b: size of the allocated memory
+ *
+ * Return: Allocated memory address
+ */
+
+ void *malloc_checked(unsigned int b)
+{
+	void *s;
+
+	s = malloc(b);
+	if (s == NULL)
+		exit(98);
+	return (s);
+}
