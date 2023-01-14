44ba76aa509a1caadbc0adcbbb82ede15dbd1c4f
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-14 05:12:08 +0300

    Make main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,5 +6,7 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int len1(char *s1);
 unsigned int len2(char *s2);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 
 #endif
