74db6c415768a29cafb0e12422ae28984da2af6a
Juliusmwash <118174651+Juliusmwash@users.noreply.github.com>
2023-01-14 05:23:54 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -13,5 +13,6 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 void errors(void);
 int _strlen(char *s);
 int is_digit(char *s);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
