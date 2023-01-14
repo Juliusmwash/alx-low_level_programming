39975d24c69cf888c86ad46f6be9dc5bc0813f24
Juliusmwash <118174651+Juliusmwash@users.noreply.github.com>
2023-01-14 05:19:51 +0300

    Update main.h

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -2,12 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
-void *malloc_checked(unsigned int b);
-char *string_nconcat(char *s1, char *s2, unsigned int n);
 unsigned int len1(char *s1);
 unsigned int len2(char *s2);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int *array_range(int min, int max);
-char *_memset(char *s, char b, unsigned int n);
 
 #endif
