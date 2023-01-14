448ff6a515d01b9941997e022951597c334581c2
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-14 17:09:08 +0300

    Correct 2-calloc.c errors (3)

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,47 +1,29 @@
 #include "main.h"
-
+#include <stdlib.h>
 /**
- * _calloc - function allocating and initializing memory with 0
- * @nmemb: number of elements
- * @size: size of the data type
+ * _calloc - allocateds memory for an array.
+ * @nmemb: number of elements.
+ * @size: size of bytes.
  *
- * Return: pointer to the allocated memory
+ * Return: pointer to the allocated memory.
+ * if nmemb or size is 0, returns NULL.
+ * if malloc fail, returns NULL.
  */
-
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int i, len;
-	char *s;
-	int *s1;
+	int *p;
+	unsigned int i;
 
-	len = nmemb * size;
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	if (size == sizeof(char))
-	{
-		s = malloc(len);
-		if (s == NULL)
-			return (NULL);
-		i = 0;
-		while (i < len)
-		{
-			*(s + i) = 0;
-			i++;
-		}
-		return (s);
-	}
-	else if (size == sizeof(int))
-	{
-		s1 = malloc(len);
-		if (s1 == NULL)
-			return (NULL);
-		i = 0;
-		while (i < len)
-		{
-			s1[i] = 0;
-			i++;
-		}
-		return (s1);
-	}
-	return (NULL);
+
+	p = malloc(nmemb * size);
+
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < (nmemb * size); i++)
+		p[i] = 0;
+
+	return (p);
 }
