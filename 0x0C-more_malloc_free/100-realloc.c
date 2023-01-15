b32f8e45adc7165ac076017bca554e0a77eb5819
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-15 14:12:58 +0300

    Correct 100-realloc.c errors (3)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,12 +15,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
 	{
-		free (ptr);
+		free(ptr);
 		return (NULL);
 	}
 	if (ptr == NULL)
 	{
 		void *arr = malloc(old_size + new_size);
+
 		if (arr == NULL)
 			return (NULL);
 		return (arr);
@@ -28,6 +29,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size > old_size)
 	{
 		void *arr = malloc(new_size);
+
 		if (arr == NULL)
 			return (ptr);
 		free(ptr);
