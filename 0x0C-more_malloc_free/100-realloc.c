8c4f2b174534f9a25d253df7df6bbe9349fe3188
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-15 13:47:52 +0300

    Correct 100-realloc.c errors (1)

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -9,11 +9,8 @@
  * Return: pointer to new memory allocated by _realloc
  */
 
-void _realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	unsigned int i;
-
-	i = 0;
 	if (new_size == old_size)
 		return (ptr);
 	if (new_size == 0 && ptr != NULL)
