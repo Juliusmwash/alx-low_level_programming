ea106a041f39f849f32046cf99558cfaa2500cf6
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-15 13:23:51 +0300

    Make 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,31 +1,40 @@
 #include "main.h"
-#include <stdlib.h>
+
 /**
- * _realloc - reallocates a memory block.
- * @ptr: pointer to the memory previously allocated.
- * @old_size: size, in bytes, of the allocated space of ptr.
- * @new_size: new size, in bytes, of the new memory block.
+ * _realloc - reallocates memory
+ * @ptr - previous allocated memory or NULL
+ * @old_size - memory previously allocated by malloc
+ * @new_size - new memory to be allocated
  *
- * Return: ptr
- * if new_size == old_size, returns ptr without changes.
- * if malloc fails, returns NULL.
+ * Return: pointer to new memory allocated by _realloc
  */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+
+void _realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	if (new_size == 0 && ptr != NULL)
-	{
-		free(ptr);
-		return (NULL);
-	}
+	unsigned int i;
 
-	if (ptr == NULL)
-		ptr = malloc(new_size);
-
+	i = 0;
 	if (new_size == old_size)
 		return (ptr);
-
-	free(ptr);
-	ptr = malloc(new_size);
-
-	return (ptr);
+	if (new_size == 0 && ptr != NULL)
+	{
+		free (ptr);
+		return (NULL);
+	}
+	if (ptr == NULL)
+	{
+		void *arr = malloc(old_size + new_size);
+		if (arr == NULL)
+			return (NULL);
+		return (arr);
+	}
+	if (new_size > old_size)
+	{
+		void *arr = malloc(new_size);
+		if (arr == NULL)
+			return (ptr);
+		free(ptr);
+		return (arr);
+	}
+	return (NULL);
 }
