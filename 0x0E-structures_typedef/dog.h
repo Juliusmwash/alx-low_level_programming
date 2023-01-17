96cb2d7aa8c98a512f5b386853360c86dee94d35
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-18 02:06:37 +0300

    Correct error not losing hope

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,8 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stdlib.h>
-
  /**
   * struct dog - contains dog information
   * @name:  First member
@@ -14,11 +12,9 @@
 
 struct dog
 {
-
 	char *name;
 	float age;
 	char *owner;
-
 };
 
 #endif
