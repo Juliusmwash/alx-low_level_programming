b45d34e5394b2025a0ecad038bcf40268c9e8929
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-18 01:40:29 +0300

    Make dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+ /**
+  * struct dog - contains dog information
+  * @name: Name of the dog
+  * @age: Age of the dog
+  * @owner: Dog owner
+  *
+  * Description: structure consisting of the name, age and the owner of the dog
+  */
+
+struct dog{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
