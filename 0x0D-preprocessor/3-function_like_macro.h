b46f21180fd7f337556ef8081ae2369d91f9261d
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-17 16:45:44 +0300

    Convert from function to macro

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,18 +1,6 @@
 #ifndef FUNCTION_LIKE_MACRO_H
 #define FUNCTION_LIKE_MACRO_H
 
-/**
- * ABS - computes absolute value of a number
- * @a: number to find its absolute value
- *
- * Return: Absolute value of a
- */
-
-int ABS(int a)
-{
-	if (a < 0)
-		return (a * -1);
-	return (a);
-}
+#define ABS(x) ((x) < (0) ? -(x) : (x))
 
 #endif
