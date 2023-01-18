6c2020258eb7c2435c4672b4bedb7a0146d8e18e
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-18 03:54:47 +0300

    Second commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,14 +10,15 @@
  * Description: Dog's information
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 
 #endif
