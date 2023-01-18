55c402b2197986edc3e377fee6d3131cffb9ace3
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-18 03:38:15 +0300

    Fourth commit

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -14,16 +14,11 @@ void print_dog(struct dog *d)
 	if (d != NULL)
 	{
 	if (d->name == NULL)
-		printf("Name: (nil)\n");
-	else
-		printf("Name: %s\n", d->name);
-	if (d->age == NULL)
-		printf("Age: (nil)\n");
-	else
-		printf("Age: %d\n", d->age);
+		d->name = "(nil)";
+	printf("Name: %s\n", d->name);
+	printf("Age: %d\n", d->age);
 	if (d->owner == NULL)
-		printf("Owner: (nil)\n");
-	else
-		printf("Owner : %s\n", d->owner);
+		d->owner = "(nil)";
+	printf("Owner : %s\n", d->owner);
 	}
 }
