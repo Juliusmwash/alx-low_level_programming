195a1f56bab1ee9938c8ddd22c1c56728cffb2e2
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-20 15:46:42 +0300

    Add header into the file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stdlib.h>
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
