accada70ce075c6c94f992b329318092dbe42980
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-20 04:28:15 +0300

    Make 2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,30 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - returns index of an integer if it meets certain conditions
+ * @array: Array of integers
+ * @size: size of the array
+ * @cmp: pointer to function
+ *
+ * Return: Nothing
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i,check
+
+	if (array != NULL && cmp != NULL)
+	{
+		if (size <= 0)
+			return (-1);
+		for (i = 0; i < size; i++)
+		{
+		check = cmp(array[i]);
+		if (check > 0)
+			return (i);
+		else
+			return (-1);
+		}
+	}
+	return (-1);
+}
