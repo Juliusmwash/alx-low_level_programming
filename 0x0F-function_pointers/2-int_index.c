af6207e9eb93b62de7496a9932e052118fb34570
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-20 16:35:19 +0300

    Fifth commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,10 +13,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i, check;
 
-	if (array ==  NULL || cmp == NULL)
+	if (array ==  NULL || cmp == NULL || size <= 0)
 		return (-1);	
-	if (size <= 0)
-		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		check = cmp(array[i]);
