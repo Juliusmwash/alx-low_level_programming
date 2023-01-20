53e6aefade9f190ac9bfc3b5b097f5776a7a368c
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-20 15:26:40 +0300

    Seventh commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,14 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Julius Mwangi
+ */
+
 #include "function_pointers.h"
 
 /**
-  * print_name - Prints a name
-  * @name: The name to prints
-  * @f: Pointer to function
-  *
-  * Return: Nothing
-  */
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-	if (name != NULL && f != NULL)
-		f(name);
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
 }
