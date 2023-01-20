4584c9a472e935c356668d7f905f1b9ea8a7f267
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-20 04:15:12 +0300

    Second commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,5 +3,7 @@
 
 int _putchar(char);
 void print_name(char *, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
