f19b606018b214ae3551eedc4f487215405c2fbc
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-20 15:54:00 +0300

    Second commit

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,11 +13,10 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	if (array != NULL && size > 0 && action != NULL)
-	{
+	if (array == NULL || action == NULL)
+		exit(0);
 	for (i = 0; i < size; i++)
 	{
 		action(*(array + i));
-	}
 	}
 }
