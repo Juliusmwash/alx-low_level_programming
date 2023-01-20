660f8ae848231f445f73ef5d7a407aee387e3dec
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-21 00:12:51 +0300

    Fifth commit

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,9 +7,19 @@ int main(int argc, char *argv[])
 	int a, b, x;
 
 	if (argc < 4)
-		exit(0);
+		exit(98);
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
+	if (b == 0 && strcmp(argv[2], "/"))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (b == 0 && strcmp(argv[2], "%"))
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	x = get_op_func(argv[2])(a, b);
 	printf("%d\n", x);
 	return (0);
