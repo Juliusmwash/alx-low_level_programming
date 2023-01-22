01a2acbece73cc312e92a4d01bb2a2ba4a22f74e
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-22 15:57:39 -0800

    Make 1-print_numbers.c

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,28 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints all the numbers given as arguments
+ * @separator: Separates the numbers as they are printed
+ * @n: count of all the numbers given
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	int i;
+	va_list args;
+
+	va_start(args, n);
+	if (separator == NULL || n == 0)
+		exit(0);
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(args, int));
+			exit(0);
+		}
+		printf("%d%s", va_arg(args, int), separator);
+	}
+}
