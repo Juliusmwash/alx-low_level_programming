f5a5fa833d187dbd117f3f6b077f988b8698a3f6
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-23 05:46:48 +0300

    Third commit

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,9 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 int _putchar(char c);
-void print_char(va_list arg);
-void print_int(va_list arg);
-void print_float(va_list arg);
-void print_string(va_list arg);
+void print_a_char(char *separator, va_list args);
+void print_a_integer(char *separator, va_list args);
+void print_a_float(char *separator, va_list args);
+void print_a_char_ptr(char *separator, va_list args);
 
 #endif
