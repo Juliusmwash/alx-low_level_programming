2dde4d4c10e43bbb2c9d5b1e7d407e21b5daf2a7
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-01-23 03:24:16 +0300

    test.c

---
+++ 0x10-variadic_functions/test.c
@@ -0,0 +1,36 @@
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - prints all the numbers given as arguments
+ * @separator: Separates the numbers as they are printed
+ * @n: count of all the numbers given
+ *
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list args;
+
+	va_start(args, n);
+	if (separator == NULL || n == 0)
+		exit(0);
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", va_arg(args, int));
+			exit(0);
+		}
+		printf("%d%s", va_arg(args, int), separator);
+	}
+}
+
+int main(void)
+{
+    print_numbers(", ", 4, 0, 98, -1024, 402);
+    return (0);
+}
