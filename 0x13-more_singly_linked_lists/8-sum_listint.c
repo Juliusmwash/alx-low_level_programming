fba6de8c34cec2a6621f6851a58d0b07fa5d7b40
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 01:50:05 +0300

    Make a function and add a prototype

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * sum_listint - adds all the data in the list;
+ * @head: first node of the list
+ * Return: sum of all the data in the list
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum;
+
+	sum = 0;
+	if (head == NULL)
+		return (0);
+	while(head)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+	return (sum);
+}
