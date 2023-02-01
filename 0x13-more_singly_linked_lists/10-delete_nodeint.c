15962a758b128b6821e044a355d33961293ee366
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-01 21:24:36 +0300

    Modify 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 	if (index > 0)
 	{
-		check = deleHelper(&temp, &temp1, index);
+		check = delete_helper(&temp, &temp1, index);
 		if (check == -1)
 			return (-1);
 	}
