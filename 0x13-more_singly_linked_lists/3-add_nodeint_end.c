7039e4554e4ca3ac23cb64b0901c8ddfdfcc35e9
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-01 22:02:40 +0300

    Modify 3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,6 +10,8 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	mem = malloc(sizeof(struct listint_s));
 	if (mem == NULL)
 		return (NULL);
+	mem->n = n;
+	mem->next = NULL;
 	temp->next = mem;
 	return (mem);
 }
