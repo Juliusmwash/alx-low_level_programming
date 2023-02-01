5244aa95cd85a59f6fc69b71c68ffd193e9799cd
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 01:36:39 +0300

    Make 7-get_nodeint.c

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - gets the required node
+ * @head: linked list first node
+ * @index: index of the required node
+ * Return: nth node
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	size_t count;
+
+	count = 0;
+	while(count <= index && head)
+	{
+		if (count == index)
+			return (head);
+		else
+		{
+			head = head->next;
+			count++;
+		}
+	}
+	return (0);
+}
