59bbac1475e47dc1267a13e90ad92bdf5b50a436
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-01 16:54:14 +0300

    Start project

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include  "lists.h"
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	int count, count1;
+	listint_t **temp, **temp1;
+
+	count = 0;
+	count1 = 0;
+	temp = head;
+	temp1 = head;
+	if (*head == 0)
+		return (-1);
+	if (index > 0)
+	{
+		while (count < index-1)
+		{
+			*temp =(*temp)->next;
+			count++;
+		}
+		while (count1 < index)
+		{
+			*temp1 = (*temp1)->next;
+			if (*temp1 == 0)
+				return (-1);
+			count1++;
+		}
+		if ((*temp1)->next == 0)
+		{
+			free((*temp1));
+			(*temp)->next = NULL;
+		}
+		else
+		{
+			(*temp)->next = (*temp1)->next;
+			free((*temp1));
+		}
+	}
+	else if (index == 0)
+	{
+		temp1 = (*tmp)->next;
+		head = temp1;
+		free((*tmp));
+	}
+	else
+		return (-1);
+
+	return (1);
+}
