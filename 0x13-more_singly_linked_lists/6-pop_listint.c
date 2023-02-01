22274dadb34c4bea15dcff57d8d420c1956a0c98
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 01:10:50 +0300

    Make 6-pop_listint.c

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,22 @@
+#include "lists.c"
+
+/**
+ * pop_listint - deletes first node
+ * @head: linked list first node
+ * Return: head's node data
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *tmp;
+	size_t data;
+
+	if (*head == NULL)
+		return (0);
+	tmp = *head;
+	*head = (*head)->next;
+	data = tmp->n;
+	free(tmp);
+	tmp = NULL;
+	return (data);
+}
