b2c536d390dc0165e5d35ab6ba603249a95c9d57
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 00:49:44 +0300

    Make 5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees list
+ * @head: pointer to linked list head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp;
+
+	while(*head)
+	{
+		tmp = *head;
+		if ((*head)->next)
+		{
+			*head = &tmp->next;
+			free(tmp);
+			tmp = NULL;
+		}
+		else
+		{
+			free(tmp);
+			*head = NULL;
+			tmp = NULL;
+		}
+	}
+}
