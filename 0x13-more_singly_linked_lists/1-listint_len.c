5e103d1b549b8d75db79e06cbdad46bde5613072
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-01 23:23:05 +0300

    Make 1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ * listint_len - calculates the number of elements in the list
+ * @h: pointer to head of the list
+ * Return: number of elements in the list
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t count;
+
+	count = 0;
+	while (h)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
