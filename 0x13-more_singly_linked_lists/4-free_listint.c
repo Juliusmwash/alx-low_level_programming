919ed51c583109cec0914e831232f9773862b761
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 00:07:17 +0300

    Add prototypes and functions

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees list
+ * @head: linked list head
+ * Return: Nothing
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while(head)
+	{
+		tmp = head;
+		head = tmp->next;
+		free(tmp);
+		tmp = NULL;
+	}
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -21,6 +21,8 @@ size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 int delete_helper(listint_t **temp, listint_t **temp1, unsigned int index);
 
