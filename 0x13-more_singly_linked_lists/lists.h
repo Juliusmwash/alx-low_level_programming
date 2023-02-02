a9a9620c8aed21f23223416fa26f287621f8ba29
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 14:11:11 -0800

    Add a prototype

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -17,6 +17,7 @@ typedef struct listint_s
     int n;
     struct listint_s *next;
 } listint_t;
+int _putchar(char);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
