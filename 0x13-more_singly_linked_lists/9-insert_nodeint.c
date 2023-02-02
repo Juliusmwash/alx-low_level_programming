74877fd87f7a7681566d87e2fb90e83361c7bcd2
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 20:58:03 +0300

    Edit 9-insert_nodeint.c

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -4,26 +4,25 @@
  * insert_nodeint_helper - performs necessary operations to enhance insertion
  * @temp: pointer to linked list
  * @temp1: pointer to linked list
- * @idx: index of where to insert listnode
  * Return: Nothing
  */
-int insert_nodeint_helper(listint_t **temp, listint_t **temp1, unsigned int idx)
+int insert_nodeint_helper(listint_t **temp,  listint_t **temp1,  unsigned int idx)
 {
 	unsigned int count;
 
 	count = 0;
-	while (count < idx)
+	while (count < idx - 1)
 	{
 		if (*temp)
 		{
 			*temp = (*temp)->next;
 			count++;
 		}
-		else
-			break;
+		if ((*temp)->next == 0)
+			return(0);
 	}
 	count = 0;
-	while (count <= idx)
+	while (count < idx)
 	{
 		if (*temp1)
 		{
@@ -46,6 +45,7 @@ int insert_nodeint_helper(listint_t **temp, listint_t **temp1, unsigned int idx)
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
+	//printf("head coming: %d\n", (*head)->n);
 	listint_t *temp, *mem, *temp1;
 	unsigned int check;
 
@@ -53,25 +53,24 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (mem == NULL)
 		return (NULL);
 	mem->n = n;
-
 	if (*head == NULL)
 	{
 		mem->next = NULL;
 		*head = mem;
 		return (mem);
 	}
-
+	if (idx == 0)
+	{
+		mem->next = *head;
+		*head = mem;
+		return(mem);
+	}
 	temp = *head;
 	temp1 = *head;
 	check = insert_nodeint_helper(&temp, &temp1, idx);
 	if (check == 0)
 		return (0);
-	if (idx == 0)
-	{
-		mem->next = temp;
-		*head = mem;
-	}
-	else
+	if (idx != 0)
 	{
 	mem->next = temp1;
 	temp->next = mem;
