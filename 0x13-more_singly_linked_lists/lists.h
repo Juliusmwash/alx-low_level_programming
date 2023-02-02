5303ecde2e7e78ccd2394e9da83230d3718edebd
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 03:48:37 +0300

    Make fun and add a proto

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -34,5 +34,6 @@ size_t looped_listint_len(const listint_t *head);
 size_t print_listint_safe(const listint_t *head);
 size_t looped_listint_count(listint_t *head);
 size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
