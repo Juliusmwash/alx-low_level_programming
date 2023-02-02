91a7783b7663691dc21e13870e576f29341854b6
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-02 03:38:21 +0300

    Make a function and add a prototype

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+  * reverse_listint - ...
+  * @head: ...
+  *
+  * Return: ...
+  */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev = NULL, *next = NULL;
+
+	if (head)
+	{
+		while (*head)
+		{
+			next = *head;
+			*head = (*head)->next;
+			next->next = prev;
+			prev = next;
+		}
+
+		*head = prev;
+		return (*head);
+	}
+
+	return (NULL);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -29,5 +29,5 @@ int sum_listint(listint_t *head);
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
 int delete_nodeint_at_index(listint_t **head, unsigned int index);
 int delete_helper(listint_t **temp, listint_t **temp1, unsigned int index);
-
+listint_t *reverse_listint(listint_t **head);
 #endif
