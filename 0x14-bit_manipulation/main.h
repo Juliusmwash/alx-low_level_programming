49ce412f92499446180afaee915c760d8bf36903
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-08 03:41:39 +0300

    Edit main.h

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 
 int _putchar(char c);
+void print_binary_helper(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
 unsigned int power(unsigned int, unsigned int);
 unsigned int _strlen(char *);
