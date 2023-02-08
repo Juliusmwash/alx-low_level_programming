7b3d09bdb62421616f8e1dcf291884052339e105
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-02-08 04:09:15 +0300

    Solving task 3.11

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -9,5 +9,9 @@ unsigned int power(unsigned int, unsigned int);
 unsigned int _strlen(char *);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
+int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int get_endianness(void);
 
 #endif
