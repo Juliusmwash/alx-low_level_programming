b5c2350daf1178e33db200d46f942d3cab73fe81
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-17 01:51:46 +0300

    Solve querry 4

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -10,9 +10,9 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	dlistint_t *newNode = malloc(sizeof(dlistint_t));
-	dlistint_t *tmp = NULL;
+	dlistint_t *newNode, *tmp;
 
+	newNode = alloc(sizeof(dlistint_t));
 	if (newNode == NULL)
 		return (NULL);
 
