aea5157fcb327a021d53db8ef8ff158c06a12919
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-16 23:58:01 +0300

    Solve querry 3

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint - creates and adds a new node to head
+ * @head: pointer to the head of the linked list
+ * @n: element to add to the linked list
+ * Return: created node or null if it fails
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *newNode= malloc(sizeof(dlistint_t));
+
+	if (newNode == NULL)
+		return (NULL);
+	newNode->n = n;
+	newNode->next = *head;
+	newNode->prev = NULL;
+	*head = newNode;
+	return (newNode);
+}
