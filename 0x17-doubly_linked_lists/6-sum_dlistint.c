954dad79a85645d8c742263bd2264f3de2b4d4d8
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-03-17 03:27:05 +0300

    Solve querry 7

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * sum-dlistint - finds sum of elements in the linked list
+ * @head: head pointer of the linked list
+ * Return: sum of the elements
+ */
+
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *tmp;
+	int sum = 0;
+
+	tmp = head;
+	if (tmp == NULL)
+		return 0;
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
+}
