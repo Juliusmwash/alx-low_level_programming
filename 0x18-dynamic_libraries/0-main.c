0996f753b290e4c8bd9e2ca7865f198fc4b3e9c7
Juliusmwash <cutejuliusmwash@gmail.com>
2023-03-21 03:23:06 +0300

    Start projects

---
+++ 0x18-dynamic_libraries/0-main.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+    printf("%d\n", _strlen("My Dyn Lib"));
+    return (EXIT_SUCCESS);
+}
