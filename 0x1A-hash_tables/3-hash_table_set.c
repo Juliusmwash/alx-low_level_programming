ce9e0189cbc4ae10337cc3df80b597091db87322
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-10 02:47:24 +0300

    3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,14 +12,16 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *tmp;
 
+	if (ht == NULL)
+		return (0);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-
 	if (strlen(key) == 0)
 		return (0);
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	index = key_index((unsigned const char *) key, ht->size);
@@ -31,6 +33,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else
 	{
+		tmp = ht->array[index];
+		while (tmp != NULL)
+		{
+		if (strcmp(tmp->key, new_node->key) == 0)
+		{
+			free(tmp->value);
+			free(new_node->key);
+			free(new_node->value);
+			free(new_node);
+			tmp->value = strdup(value);
+			return (1);
+		}
+		}
 		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
