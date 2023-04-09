fe1610bb411756a70f48462e9e89211b449aa6c6
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-10 02:19:56 +0300

    Fix error

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,76 +1,38 @@
 #include "hash_tables.h"
+#include <string.h>
+
 /**
- * hash_table_set - adds an element to the hash table.
+ * hash_table_set - adds an element to the hash table
  * @ht: hash table
- * @key: is the key. key can not be an empty string
- * @value: value associated with the key.
- * value must be duplicated. value can be an empty string
- * Return: 1 on success, 0 on failurre
+ * @key: string key
+ * @value: string value
+ * Return: 1 or 0
  */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-
-hash_node_t *node;
-hash_node_t *new_node;
-unsigned long int index;
-
-if (ht == NULL || *key == '\n' || *value == '\n')
-	return (0);
-
-index = key_index((const unsigned char *)key, ht->size);
-node = ht->array[index];
-
-if (node == NULL)
-{
-	new_node = create_new_node(key, value);
-	if (new_node == NULL)
-		return (0);
-
-	ht->array[index] = new_node;
-	return (1);
-}
-
-/*If key exists, replace value*/
-while (node != NULL)
-{
-	if (strcmp(key, node->key) == 0)
-	{
-		free(node->value);
-		node->value = strdup(value);
-		return (1);
-	}
-	node = node->next;
-}
-/*If key doesn't exist, create new node*/
-new_node = create_new_node(key, value);
-if (new_node == NULL)
-	return (0);
-
-new_node->next = ht->array[index];
-ht->array[index] = new_node;
-return (1);
-}
-
-/**
- * create_new_node - create a new node
- * @key: is the key. key can not be an empty string
- * @value: value associated with the key.
- * value must be duplicated. value can be an empty string
- * Return: 1 on success, 0 on failurre
- */
-
-hash_node_t *create_new_node (const char *key, const char *value)
-{
+	unsigned long int index;
 	hash_node_t *new_node;
 
 	new_node = malloc(sizeof(hash_node_t));
-
 	if (new_node == NULL)
-		return (NULL);
+		return (0);
 
+	if (strlen(key) == 0)
+		return (0);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
-	new_node->next = NULL;
+	index = key_index((unsigned const char *) key, ht->size);
 
-	return (new_node);
+	if (ht->array[index] == NULL)
+	{
+		new_node->next = NULL;
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
+	return (1);
 }
