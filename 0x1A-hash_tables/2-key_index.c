a5f894bed68bf4efeecf9239bb3a30a778432b88
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-10 00:53:58 +0300

    Fix an error

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -1,14 +1,18 @@
-#include "hash_tables.h"
+#include "hash_table.h"
 
 /**
- * key_index - returns the index of a key
- * @key: key
- * @size: size of the array of the hash table
- * Return: index
+ * key_index - generates index for the hash table
+ * @key: string in which index is generated from
+ * @size: size of the hash table
+ * Return: generated index
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int index = hash_djb2(key) % size;
+	unsigned long int num, index;
+
+	num = hash_djb2(key);
+	index = num % size;
 
 	return (index);
 }
