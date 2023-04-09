181ab5acf04423df6403375c7d93cf212239e084
Julius Mwangi <cutejuliusmwash@gmail.com>
2023-04-09 22:21:27 +0300

    Fix error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -10,14 +10,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i = 0;
 
 	hash_table = malloc(sizeof(hash_table_t));
-	hash_table->size = size;
 	if (hash_table == NULL)
 		return (NULL);
+	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (hash_table->array == NULL)
+	{
+		free(hash_table);
 		return (NULL);
+	}
+	for (; i < size; i++)
+		hash_table->array[i] = NULL;
 
 	return (hash_table);
 }
