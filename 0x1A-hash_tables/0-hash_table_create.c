5352ba6136cfc3c77ee9f4cd8d805207737342c8
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-09 15:30:09 +0300

    Fix error

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,27 +1,23 @@
+#include <stdlib.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - creates a hash table
- * @size: size of the array
- *
- * Return: pointer to the newly created hash table
+ * @size: size of the hash table
+ * Return: pointer to created hash table of null creation fails
  */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
-	unsigned long int i;
 
 	hash_table = malloc(sizeof(hash_table_t));
+	hash_table->size = size;
 	if (hash_table == NULL)
 		return (NULL);
-	hash_table->size = size;
-	hash_table->array = malloc(size * sizeof(hash_node_t *));
+	hash_table->array = malloc(sizeof(hash_node_t) * size);
 	if (hash_table->array == NULL)
-	{
-		free(hash_table);
 		return (NULL);
-	}
-	for (i = 0; i < size; i++)
-		hash_table->array[i] = NULL;
+
 	return (hash_table);
 }
