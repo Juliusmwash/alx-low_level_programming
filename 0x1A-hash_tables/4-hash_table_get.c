5d1a95015245c62aac5f700831d27ffcd7bcc8b9
Juliusmwash <cutejuliusmwash@gmail.com>
2023-04-10 14:07:21 +0300

    Fix error

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,28 +1,27 @@
 #include "hash_tables.h"
+
+
 /**
- * hash_table_get - retrieves a value associated with a key.
- * @ht: the hash table you want to look into
- * @key: is the key you are looking for
- * Return: value associated with key if found, NULL if failed
+ * hash_table_get - gets the value associated with the key
+ * @ht: the hash table
+ * @key: the key, string
+ * Return: value of the key
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int index;
+	hash_node_t *tmp;
 
-hash_node_t *node;
-unsigned long int index;
-
-if (ht == NULL)
+	if (ht == NULL)
+		return (NULL);
+	index = key_index((const unsigned char *) key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+			return (tmp->value);
+		tmp = tmp->next;
+	}
 	return (NULL);
-
-index = key_index((const unsigned char *) key, ht->size);
-node = ht->array[index];
-
-while (node != NULL)
-{
-	if (strcmp(node->key, key) == 0)
-		return (node->value);
-
-	node = node->next;
-}
-return (NULL);
 }
