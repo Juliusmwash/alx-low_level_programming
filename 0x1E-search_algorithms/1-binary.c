0711325f6f6ecec36a057b3c70fc6b17ace4482d
Juliusmwash <cutejuliusmwash@gmail.com>
2023-07-11 19:00:01 +0300

    Add a condition to exit if the array is NULL

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -49,6 +49,8 @@ int binary_search(int *array, size_t size, int value)
 {
 	int index;
 
+	if (array == NULL)
+		return (-1);
 	index = binary_search_helper(0, size - 1, value, array);
 	return (index);
 }
